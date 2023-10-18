1f829aaa444ad50413c631342ae99e88f8edb2d8
abakum <koka.abakum@gmail.com>
2023-10-18 11:42:07 +0300

    first

---
+++ EnRu.ahk
@@ -0,0 +1,44 @@
+﻿;https://github.com/abakum/EnRu.git
+;Another global keyboard layout switch by clicking the left or right Ctrl key
+;Еще один глобальный переключатель раскладки клавиатуры щелчком левой или правой клавиши Ctrl
+
+#Requires AutoHotkey v2.0
+#SingleInstance
+;@Ahk2Exe-AddResource 1.ico, 1
+;@Ahk2Exe-AddResource 2.ico, 2
+;@Ahk2Exe-SetMainIcon EnRu.ico
+
+hkl:=[0x4090409, 0x4190419]
+WM_InputLangChangeRequest:=0x0050
+HWND_BroadCast:=0xFFFF
+Frequency:=523
+Timer:=-500
+; Item "En", 1, A_TrayMenu
+
+~LControl up::{
+ if "LControl"=A_PriorKey
+  Item "En", 1, A_TrayMenu
+}
+
+~RControl up::{
+ if "RControl"=A_PriorKey
+  Item "Ru", 2, A_TrayMenu
+}
+
+A_TrayMenu.Insert "1&"
+A_TrayMenu.Insert "1&", "Ru", Item
+A_TrayMenu.Insert "1&", "En", Item
+
+Item(ItemName, ItemPos, MyMenu) {
+ ToolTip ItemName
+ PostMessage WM_InputLangChangeRequest,,hkl[ItemPos],, HWND_BroadCast
+ ;@Ahk2Exe-IgnoreBegin
+  TraySetIcon ItemPos ".ico"
+ ;@Ahk2Exe-IgnoreEnd
+ /*@Ahk2Exe-Keep
+  TraySetIcon A_ScriptName, -ItemPos
+ */
+ A_IconTip:=ItemName
+ SoundBeep Frequency*ItemPos
+ SetTimer () => ToolTip(), Timer
+}
---
+++ ico.bat
@@ -0,0 +1,110 @@
+chcp 65001
+set p=
+set v=0\
+set t=Z
+set c=#0000FF
+call :a
+set t=Я
+set c=#FF0000
+call :a
+
+set v=1\
+md %v%
+set c=blue
+set t=En
+set x=(w-text_w)/2
+set o=1
+call :b
+set c=red
+set t=Ru
+set o=2
+call :b
+call :aa
+pause
+
+set v=2\
+md %v%
+set c=blue
+set t=Z
+set x=1
+set o=1
+call :b
+set c=red
+set t=Я
+set x=w-text_w
+set o=2
+call :b
+call :bb
+pause
+
+set v=3\
+md %v%
+set c=blue
+set t=R
+set x=0
+set o=1
+call :b
+set c=red
+set t=Я
+set x=w-text_w
+set o=2
+call :b
+call :cc
+pause
+
+set v=4\
+md %v%
+set c=red
+set t=Z
+set x=1
+set p=:boxborderw=9:box=1:boxcolor=blue
+set o=1
+call :b
+set c=blue
+set t=Я
+set x=w-text_w
+set p=:boxborderw=5:box=1:boxcolor=red
+set o=2
+call :b
+call :bb
+pause
+
+set v=5\
+md %v%
+set c=red
+set t=R
+set x=0
+set p=:boxborderw=6:box=1:boxcolor=blue
+set o=1
+call :b
+set c=blue
+set t=Я
+set x=w-text_w
+set p=:boxborderw=6:box=1:boxcolor=red
+set o=2
+call :b
+call :cc
+
+goto :EOF
+
+:a
+echo https://redketchup.io/icon-editor arial 22 bold %t% #FFFFFF on %c%
+goto :EOF
+
+:b
+ffmpeg -f lavfi -i color=%c%:size=32x32 -frames:v 1 -filter_complex drawtext=text=%t%:font=impact:fontcolor=white:fontsize=32:x=%x%:y=(h-text_h)/2:shadowx=3:shadowy=2%p% -y %v%%o%.ico
+goto :EOF
+
+
+:aa
+ffmpeg -i %v%1.ico -i %v%2.ico -filter_complex [0]crop=iw/2-1:ih:0:0[l];[1]crop=iw/2+1:ih:0:0[r];[l][r]hstack -y %v%EnRu.ico
+goto :EOF
+
+:bb
+ffmpeg -i %v%1.ico -i %v%2.ico -filter_complex [0]crop=iw/2-1:ih:0:0[l];[1]crop=iw/2+1:ih:iw/2-1:0[r];[l][r]hstack -y %v%EnRu.ico
+goto :EOF
+
+:cc
+ffmpeg -i %v%1.ico -i %v%2.ico -filter_complex [0]crop=iw/2:ih:0:0[l];[1]crop=iw/2:ih:iw/2:0[r];[l][r]hstack -y %v%EnRu.ico
+goto :EOF
+
